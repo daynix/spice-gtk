30f9a69efdfe0dae50db19c7d184239699db4f97
Alexander Nezhinsky <alexander@daynix.com>
2018-07-23 18:58:05 +0300

    cd-scsi-dev-params.h: add copyright notice

--- src/cd-scsi-dev-params.h
+++ src/cd-scsi-dev-params.h
@@ -1,4 +1,25 @@
-/* cd-scsi-params.h */
+/* -*- Mode: C; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+   CD SCSI device parameters
+
+   Copyright (C) 2018 Red Hat, Inc.
+
+   Red Hat Authors:
+   Alexander Nezhinsky<anezhins@redhat.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef _CD_SCSI_DEV_PARAMS_H_
 #define _CD_SCSI_DEV_PARAMS_H_
